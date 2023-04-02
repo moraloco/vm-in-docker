baf1c2ad40835b40795478654c7f2fac2c8ba01c
Mark Philip Mosskov Ebbesen <mark.ebbesen@gmail.com>
2023-04-02 14:22:28 +0200

    added ip-adress lookup for vagrant box in startup.sh

--- win10-enterprise/startup.sh
+++ win10-enterprise/startup.sh
@@ -5,6 +5,7 @@ chown root:kvm /dev/kvm
 service libvirtd start
 service virtlogd start
 VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up
+IP_ADDRESS=$(vagrant ssh-config | awk '/HostName/ { print $2 }')
 iptables-save > $HOME/firewall.txt
 iptables -X
 iptables -t nat -F
@@ -18,8 +19,8 @@ iptables -P OUTPUT ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -p tcp --syn --dport 3389 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i eth0 -o virbr1 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i virbr1 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination 192.168.121.68
-iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d 192.168.121.68 -j SNAT --to-source 192.168.121.1
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 3389 -j DNAT --to-destination $IP_ADDRESS
+iptables -t nat -A POSTROUTING -o virbr1 -p tcp --dport 3389 -d $IP_ADDRESS -j SNAT --to-source $IP_ADDRESS
 
 iptables -D FORWARD -o virbr1 -j REJECT --reject-with icmp-port-unreachable
 iptables -D FORWARD -i virbr1 -j REJECT --reject-with icmp-port-unreachable
